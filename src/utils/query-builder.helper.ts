84c244c7815e957d93556752d515fb7fd3570b9b
Sejal Dhawan <sejaldhawan@cvpcorp.com>
2021-04-27 10:23:17 -0400

    added allowance API filters

--- src/utils/query-builder.helper.ts
+++ src/utils/query-builder.helper.ts
@@ -1,3 +1,5 @@
+import { Regex } from './regex';
+
 export class QueryBuilderHelper {
   private static paginationHelper(query: any, page: number, perPage: number) {
     query.skip((page - 1) * perPage).take(perPage);
@@ -14,6 +16,56 @@ export class QueryBuilderHelper {
     if (param.includes('vintageYear') && dto.vintageYear) {
       query.andWhere(`${allowanceAlias}.vintageYear IN (:...vintageYears)`, {
         vintageYears: dto.vintageYear,
+      });
+    }
+
+    if (param.includes('accountNumber') && dto.accountNumber) {
+      query.andWhere(
+        `UPPER(${allowanceAlias}.accountNumber) IN (:...accountNumber)`,
+        {
+          accountNumber: dto.accountNumber.map(accountNumber => {
+            return accountNumber.toUpperCase();
+          }),
+        },
+      );
+    }
+
+    if (param.includes('orisCode') && dto.orisCode) {
+      query.andWhere(`${accountAlias}.orisCode IN (:...orisCodes)`, {
+        orisCodes: dto.orisCode,
+      });
+    }
+
+    if (param.includes('ownerOperator') && dto.ownerOperator) {
+      let string = '(';
+
+      for (let i = 0; i < dto.ownerOperator.length; i++) {
+        const regex = Regex.commaDelimited(dto.ownerOperator[i].toUpperCase());
+
+        if (i === 0) {
+          string += `(UPPER(${accountAlias}.ownDisplay) ~* ${regex}) `;
+        } else {
+          string += `OR (UPPER(${accountAlias}.ownDisplay) ~* ${regex}) `;
+        }
+      }
+
+      string += ')';
+      query.andWhere(string);
+    }
+
+    if (param.includes('state') && dto.state) {
+      query.andWhere(`UPPER(${accountAlias}.state) IN (:...states)`, {
+        states: dto.state.map(state => {
+          return state.toUpperCase();
+        }),
+      });
+    }
+
+    if (param.includes('program') && dto.program) {
+      query.andWhere(`UPPER(${allowanceAlias}.prgCode) IN (:...programs)`, {
+        programs: dto.program.map(program => {
+          return program.toUpperCase();
+        }),
       });
     }
 
