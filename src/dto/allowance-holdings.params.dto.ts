fdb603abcc6d60f579b4d56e4ec3b82a9675d734
Junie-CVP <juniesenthilkumar@cvpcorp.com>
2021-08-16 13:36:52 -0400

    refactored holdings and transactions to avoid code redundancy

--- src/dto/allowance-holdings.params.dto.ts
+++ src/dto/allowance-holdings.params.dto.ts
@@ -1,68 +1,15 @@
 import { Transform } from 'class-transformer';
 import { IsOptional } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
 
-import { PaginationDTO } from './pagination.dto';
 import { ActiveAllowanceProgram } from '../enum/active-allowance-program.enum';
-import { State } from '../enum/state.enum';
-import { AccountType } from '../enum/account-type.enum';
-import { IsOrisCode } from '../pipes/is-oris-code.pipe';
-import { IsStateCode } from '../pipes/is-state-code.pipe';
-import { IsAccountType } from '../pipes/is-account-type.pipe';
-import { IsAccountNumber } from '../pipes/is-account-number.pipe';
-import { IsYearGreater } from '../pipes/is-year-greater.pipe';
 import { ErrorMessages } from '../utils/error-messages';
-import { IsYearFormat } from '../pipes/is-year-format.pipe';
 import { IsAllowanceProgram } from '../pipes/is-allowance-program.pipe';
+import { AllowanceParamsDTO } from './allowance.params.dto';
 
-export class AllowanceHoldingsParamsDTO extends PaginationDTO {
-  @IsOptional()
-  @IsAccountType({
-    each: true,
-    message: ErrorMessages.AccountCharacteristics(true, 'accountType'),
-  })
-  @Transform((value: string) => value.split('|').map(item => item.trim()))
-  accountType?: AccountType[];
-
-  @IsOptional()
-  @IsAccountNumber({
-    each: true,
-    message: ErrorMessages.AccountCharacteristics(true, 'accountNumber'),
-  })
-  @Transform((value: string) => value.split('|').map(item => item.trim()))
-  accountNumber?: string[];
-
-  @IsOptional()
-  @IsOrisCode({
-    each: true,
-    message: ErrorMessages.AccountCharacteristics(true, 'orisCode'),
-  })
-  @Transform((value: string) => value.split('|').map(item => item.trim()))
-  orisCode?: number[];
-
+export class AllowanceHoldingsParamsDTO extends AllowanceParamsDTO {
   @IsOptional()
   @Transform((value: string) => value.split('|').map(item => item.trim()))
   ownerOperator?: string[];
-
-  @IsOptional()
-  @IsStateCode({
-    each: true,
-    message: ErrorMessages.AccountCharacteristics(true, 'state'),
-  })
-  @Transform((value: string) => value.split('|').map(item => item.trim()))
-  state?: State[];
-
-  @IsOptional()
-  @IsYearFormat({
-    each: true,
-    message: ErrorMessages.MultipleFormat('vintageYear', 'YYYY'),
-  })
-  @IsYearGreater(1995, {
-    each: true,
-    message: ErrorMessages.YearRange('vintageYear', '1995'),
-  })
-  @Transform((value: string) => value.split('|').map(item => item.trim()))
-  vintageYear?: number[];
 
   @IsOptional()
   @IsAllowanceProgram(true, {
@@ -73,9 +20,4 @@ export class AllowanceHoldingsParamsDTO extends PaginationDTO {
   })
   @Transform((value: string) => value.split('|').map(item => item.trim()))
   program?: ActiveAllowanceProgram[];
-
-  @ApiProperty({
-    description: 'Attaches a file with data in the format specified by the Accept header',
-  })
-  attachFile?: boolean;
 }
--- src/dto/allowance-transactions.params.dto.ts
+++ src/dto/allowance-transactions.params.dto.ts
@@ -1,23 +1,14 @@
 import { Transform } from 'class-transformer';
 import { IsOptional } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
 
-import { PaginationDTO } from './pagination.dto';
-import { State } from '../enum/state.enum';
-import { AccountType } from '../enum/account-type.enum';
-import { IsOrisCode } from '../pipes/is-oris-code.pipe';
-import { IsStateCode } from '../pipes/is-state-code.pipe';
-import { IsAccountType } from '../pipes/is-account-type.pipe';
-import { IsAccountNumber } from '../pipes/is-account-number.pipe';
+import { AllowanceParamsDTO } from './allowance.params.dto';
 import { ErrorMessages } from '../utils/error-messages';
-import { IsYearFormat } from '../pipes/is-year-format.pipe';
 import { TransactionType } from '../enum/transaction-type.enum';
 import { AllowanceProgram } from '../enum/allowance-programs.enum';
-import { IsYearGreater } from '../pipes/is-year-greater.pipe';
 import { BeginDate, EndDate } from '../utils/validator.const';
 import { IsAllowanceProgram } from '../pipes/is-allowance-program.pipe';
 
-export class AllowanceTransactionsParamsDTO extends PaginationDTO {
+export class AllowanceTransactionsParamsDTO extends AllowanceParamsDTO {
   @IsOptional()
   @IsAllowanceProgram(false, {
     each: true,
@@ -29,52 +20,8 @@ export class AllowanceTransactionsParamsDTO extends PaginationDTO {
   program?: AllowanceProgram[];
 
   @IsOptional()
-  @IsAccountType({
-    each: true,
-    message: ErrorMessages.AccountCharacteristics(true, 'accountType'),
-  })
-  @Transform((value: string) => value.split('|').map(item => item.trim()))
-  accountType?: AccountType[];
-
-  @IsOptional()
-  @IsAccountNumber({
-    each: true,
-    message: ErrorMessages.AccountCharacteristics(true, 'accountNumber'),
-  })
-  @Transform((value: string) => value.split('|').map(item => item.trim()))
-  accountNumber?: string[];
-
-  @IsOptional()
-  @IsOrisCode({
-    each: true,
-    message: ErrorMessages.AccountCharacteristics(true, 'orisCode'),
-  })
-  @Transform((value: string) => value.split('|').map(item => item.trim()))
-  orisCode?: number[];
-
-  @IsOptional()
   @Transform((value: string) => value.split('|').map(item => item.trim()))
   ownerOperator?: string[];
-
-  @IsOptional()
-  @IsStateCode({
-    each: true,
-    message: ErrorMessages.AccountCharacteristics(true, 'state'),
-  })
-  @Transform((value: string) => value.split('|').map(item => item.trim()))
-  state?: State[];
-
-  @IsOptional()
-  @IsYearFormat({
-    each: true,
-    message: ErrorMessages.MultipleFormat('vintageYear', 'YYYY'),
-  })
-  @IsYearGreater(1995, {
-    each: true,
-    message: ErrorMessages.YearRange('vintageYear', '1995'),
-  })
-  @Transform((value: string) => value.split('|').map(item => item.trim()))
-  vintageYear?: number[];
 
   @BeginDate()
   transactionBeginDate: Date;
@@ -85,10 +32,4 @@ export class AllowanceTransactionsParamsDTO extends PaginationDTO {
   @IsOptional()
   @Transform((value: string) => value.split('|').map(item => item.trim()))
   transactionType?: TransactionType[];
-
-  @ApiProperty({
-    description:
-      'Attaches a file with data in the format specified by the Accept header',
-  })
-  attachFile?: boolean;
 }
