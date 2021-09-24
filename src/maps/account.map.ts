2663f34bf03ce6529f0de70447724043906dd19d
Sejal Dhawan <sejaldhawan@cvpcorp.com>
2021-09-24 16:02:20 -0400

    fix nercRegion

--- src/maps/account.map.ts
+++ src/maps/account.map.ts
@@ -18,9 +18,7 @@ export class AccountMap extends BaseMap<AccountFact, any> {
         epaRegion: entity.epaRegion
           ? Number(entity.epaRegion)
           : entity.epaRegion,
-        nercRegion: entity.nercRegion
-          ? Number(entity.nercRegion)
-          : entity.nercRegion,
+        nercRegion: entity.nercRegion,
       };
     }
     return {
