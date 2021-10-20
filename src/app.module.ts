0bb90fc07bb2cc48b02937b7e77cfdab4443c28a
ACPR <yefimabramson@cvpcorp.com>
2021-10-20 15:58:16 -0400

    added shared library cors config

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,8 @@ import routes from './routes';
 import dbConfig from './config/db.config';
 import appConfig from './config/app.config';
 import { TypeOrmConfigService } from './config/typeorm.config';
+import { LoggerModule } from '@us-epa-camd/easey-common/logger/Logger.module';
+import { CorsOptionsModule } from '@us-epa-camd/easey-common/cors-options/cors-options.module';
 import { AllowanceHoldingsModule } from './allowance-holdings/allowance-holdings.module';
 import { AccountModule } from './account/account.module';
 import { AllowanceTransactionsModule } from './allowance-transactions/allowance-transactions.module';
@@ -23,6 +25,8 @@ import { EmissionsComplianceModule } from './emissions-compliance/emissions-comp
     TypeOrmModule.forRootAsync({
       useClass: TypeOrmConfigService,
     }),
+    LoggerModule,
+    CorsOptionsModule,
     AllowanceHoldingsModule,
     AllowanceTransactionsModule,
     AccountModule,
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { CorsOptionsService } from '@us-epa-camd/easey-common/cors-options/cors-options.service';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   const configService = app.get(ConfigService);
+  const corsOptionsService = app.get(CorsOptionsService);
 
+  const appName = configService.get<string>('app.name');
   const appTitle = configService.get<string>('app.title');
   const appPath = configService.get<string>('app.path');
   const appEnv = configService.get<string>('app.env');
@@ -28,10 +32,9 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix(appPath);
-  app.enableCors({
-    origin: '*',
-    exposedHeaders: '*',
-    methods: '*'
+
+  app.enableCors(async (req, callback) => {
+    await corsOptionsService.configure(req, appName, callback);
   });
 
   const swaggerDocOptions = new DocumentBuilder()
