b5619577fb2d4b6744bbb0f766ed0a9497b951d2
abufofanah <cvp@Abus-MBP.lan>
2023-07-20 11:33:27 -0400

    updated easey common

--- src/allowance-holdings/allowance-holdings.service.ts
+++ src/allowance-holdings/allowance-holdings.service.ts
@@ -18,7 +18,7 @@ import {
 } from '../constants/field-mappings';
 import { Logger } from '@us-epa-camd/easey-common/logger';
 import { ApplicableAllowanceHoldingsAttributesDTO } from '../dto/applicable-allowance-holdings-attributes.dto';
-import { LoggingException } from '@us-epa-camd/easey-common/exceptions';
+import { EaseyException } from '@us-epa-camd/easey-common/exceptions';
 
 @Injectable()
 export class AllowanceHoldingsService {
@@ -32,7 +32,7 @@ export class AllowanceHoldingsService {
     paginatedAllowanceHoldingsParamsDTO: PaginatedAllowanceHoldingsParamsDTO,
     req: Request,
   ): Promise<AllowanceHoldingsDTO[]> {
-    this.logger.info('Getting allowance holdings');
+    this.logger.log('Getting allowance holdings');
     let query;
     try {
       query = await this.allowanceHoldingsRepository.getAllowanceHoldings(
@@ -40,7 +40,7 @@ export class AllowanceHoldingsService {
         req,
       );
     } catch (e) {
-      throw new LoggingException(e.message, HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new EaseyException(new Error(e.message), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     req.res.setHeader(
@@ -51,22 +51,22 @@ export class AllowanceHoldingsService {
       excludableColumnHeader,
       JSON.stringify(fieldMappings.allowances.holdings.excludableColumns),
     );
-    this.logger.info('Got allowance holdings');
+    this.logger.log('Got allowance holdings');
     return this.allowanceHoldingsMap.many(query);
   }
 
   async getAllApplicableAllowanceHoldingsAttributes(): Promise<
     ApplicableAllowanceHoldingsAttributesDTO[]
   > {
-    this.logger.info('Getting all applicable allowance holding attributes');
+    this.logger.log('Getting all applicable allowance holding attributes');
     let query;
     try {
       query = await this.allowanceHoldingsRepository.getAllApplicableAllowanceHoldingsAttributes();
     } catch (e) {
-      throw new LoggingException(e.message, HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new EaseyException(new Error(e.message), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    this.logger.info('Got all applicable allowance holding attributes');
+    this.logger.log('Got all applicable allowance holding attributes');
 
     return query.map(item => {
       return plainToClass(ApplicableAllowanceHoldingsAttributesDTO, item, {
